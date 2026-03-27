c75835efdb6a80e1768c0da29337fa0e000dbcbe
Gabo1234567890 <gabonenov@gmail.com>
2026-03-27 14:25:10 +0200

    fix minor visual bug

--- green-win/src/components/landing-page/how-it-worrks-card.tsx
+++ green-win/src/components/landing-page/how-it-worrks-card.tsx
@@ -14,7 +14,7 @@ export default function HowItWorksCard({
   return (
     <div className="flex flex-row items-end gap-7 p-7 rounded-lg bg-base-100 shadow-custom">
       <p className="heading1 text-base-900">{number}</p>
-      <div className="flex flex-col gap-0 items-center justify-center h-full">
+      <div className="flex flex-col gap-0 h-full">
         <div className="flex flex-row gap-3">
           <Icon src={src}></Icon>
           <p className="text-lg heading6 text-base-900">{title}</p>
